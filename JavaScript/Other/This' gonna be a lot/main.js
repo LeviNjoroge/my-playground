83a25c41498a9047bdd4b18f2c415429f690dfde
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-25 14:40:00 +0300

    working on javascript

--- JavaScript/Other/This' gonna be a lot/main.js
+++ JavaScript/Other/This' gonna be a lot/main.js
@@ -74,3 +74,12 @@
     }
     while(y < 5);
 }
+
+// Functions
+{
+    console.log("Addition of two numbers using function:");
+    function add(num1, num2) {
+        return num1+num2;
+    }
+    console.log(add(10,20));
+}
