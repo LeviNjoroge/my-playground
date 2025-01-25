3d76b61e71d7d35dbf635faff6f7a8d7a6486716
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-25 13:52:48 +0300

    working on javascript

--- JavaScript/Other/This' gonna be a lot/main.js
+++ JavaScript/Other/This' gonna be a lot/main.js
@@ -6,7 +6,44 @@
     if(num1>num2){
         console.log("num1 is greater");
     }
-    else{
+    else if(num1<num2){
         console.log("num2 is greater");
+    }
+    else{
+        console.log("Both are equal");
+    }
+}
+
+// 2. Switch case
+{
+    console.log("The day is:");
+    let day = Math.ceil((Math.random())*7);
+    console.log(day);
+    switch(day){
+        case 1:
+            console.log("Monday");
+            break;
+        
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5: 
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+        case 7:
+            console.log("Sunday");
+            break;
+        default:
+            console.log("Invalid Day");
+            break;
     }
 }
