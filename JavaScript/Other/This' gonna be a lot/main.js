329de69a0a5c470a00e7c8d57969e30b8954fd93
Levi Njoroge <levynjoroge1@gmail.com>
2025-01-25 13:41:39 +0300

    working on javascript

---
+++ JavaScript/Other/This' gonna be a lot/main.js
@@ -0,0 +1,12 @@
+// 1. If statements
+{
+    let num1 = 10;
+    let num2 = 12;
+
+    if(num1>num2){
+        console.log("num1 is greater");
+    }
+    else{
+        console.log("num2 is greater");
+    }
+}
