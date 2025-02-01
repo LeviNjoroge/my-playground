dc8b915c3e9852ccf82a15ce1f48654aa0a9ed05
Levi Njoroge <levynjoroge1@gmail.com>
2025-02-01 22:45:45 +0300

    working on a java task on unit conversion

--- Java/Units Convertor/Main.java
+++ Java/Units Convertor/Main.java
@@ -2,35 +2,38 @@ import java.util.*;
 
 public class Main {
 
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         
-        int result = 0;
+        double result = 0;
         System.out.println("Welcome to the unit converter!\nSelect category:\n1. Length. \n2. Weight. \n3. Temperature. ");
         int category = input.nextInt();
         switch(category){
-            case 1: 
-            {
-                System.out.println("Choose an option: \n"+
+            case 1:
+            lenConversion();
+            break;
+        }
+    }
+
+    static double lenConversion(){
+        System.out.println("Choose an option: \n"+
                                         "1. Centimeters to meters.\n" +
                                         "2. Meters to centimeters\n" +
                                         "3. Centimeters to inches.\n" +
                                         "4. Inches to Centimeters\n" +
                                         "5. Meters to feet\n" +
                                         "6. Feet to Meters");
-                int option = input.nextInt();
-                switch (option) {
-                    case 1:
-                        System.out.println("Enter the value in centimeters to convert: ");
-                        int value = input.nextInt();
-                        result = value/100;
-                        break;
-                
-                    default:
-                        break;
+        int option = input.nextInt();
+        System.out.println("Enter the value to convert: ");
+        int value = input.nextInt();
+        switch (option) {
+            case 1:
+                return value/100;
+            case 2:
+                return value*100;
+            default:
+                break;
                 }
-            }
-            break;
-        }
     }
 }
