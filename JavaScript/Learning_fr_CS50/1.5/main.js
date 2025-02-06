62cfb8c14ed1450629d70dc30a53f42798742770
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-06 16:02:30 +0300

    working on the js project wit harvard's prof: Brayo

--- JavaScript/Learning_fr_CS50/1.5/main.js
+++ JavaScript/Learning_fr_CS50/1.5/main.js
@@ -1,3 +1,7 @@
-document.querySelector("h1").addEventListener("click", function(){
-    this.innerHTML = "Hello!"
+// document.querySelector("h1").addEventListener("click", function(){
+//     this.innerHTML = "Hello!"
+// })
+
+document.querySelector('h1').addEventListener('click', (event) => {
+    event.target.innerHTML = "Bonjour";
 })
