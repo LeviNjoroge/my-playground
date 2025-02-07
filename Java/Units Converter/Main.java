0538d2dbdb25a49eb136d7b50c08cad5345b8752
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-07 20:13:57 +0300

    working on a project in java

--- Java/Units Converter/Main.java
+++ Java/Units Converter/Main.java
@@ -57,7 +57,7 @@ public class Main {
             case 6:
                 return value/METERS_TO_FEET;
             default:
-                System.out.println("Invalid option chosen!");
+                System.out.println("Invalid selection!");
                 return 0;
                 }
     }
