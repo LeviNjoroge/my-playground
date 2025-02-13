988dd3b992cc9144d322d533a23d74bc4725956f
Levi Njoroge <Levynjoroge1@gmail.com>
2025-02-13 15:54:41 +0300

    working on some tasks with CS50's lec

--- JavaScript/Learning_fr_CS50/1.7 - local storage/counter.js
+++ JavaScript/Learning_fr_CS50/1.7 - local storage/counter.js
@@ -1,13 +1,18 @@
-if (!localStorage('count')) {
+if (!localStorage.getItem('count')) {
     localStorage.setItem('count', 0);
 }
 
 const button = document.querySelector('button');
 const hero = document.querySelector('h1');
+let count = localStorage.getItem('count');
 function counter(){
-    let count = localStorage.getItem('count');
     count++;
     hero.innerHTML = count;
+    localStorage.setItem('count', count);
 }
 
 setInterval(counter, 1000);
+
+document.addEventListener('DOMContentLoaded', function(){
+    hero.innerHTML = localStorage.getItem('count');
+});
