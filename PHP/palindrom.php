597aed4989d05e75e76bbc47f7583c226a9cb99e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-08 12:12:29 +0300

    still learning php

---
+++ PHP/palindrom.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Palindrometer</title>
+</head>
+<body>
+    <h1>This is a Palindrometer</h1>
+    <form action="palindrom.php">
+        <label for="text">Enter some text to see if it's a palindrom:</label><br>
+        <input type="text" name="text" id="text"> <br>
+        <input type="submit" value="Test">
+    </form>
+</body>
+</html>
