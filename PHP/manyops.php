6b5efdb8560f5b4a1b0cff2733e4445017db1694
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-08 12:03:44 +0300

    still learning php

--- PHP/manyops.php
+++ PHP/manyops.php
@@ -8,14 +8,16 @@
 <body>
     <form action="manyops.php">
         <label for="string">Enter a string(this can be anything from a sentence to a word):</label><br>
-        <input type="text" name="string" id="string"> <br>
+        <input type="text" name="string" id="string"> <br> <br>
 
         <?php
-        
-        ?>
-        <p>
+        $str = $_GET["string"];
+        echo "The lenth of the text is: ".strlen($str)." characters.";
+        echo "In lower, the test is: ". strtolower($str);
+        echo "In uppercase: ".strtoupper($str);
+        echo "With first letters in upper: "
 
-        </p>
+        ?>
     </form>
 </body>
 </html>
