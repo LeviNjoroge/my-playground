122af993f5dbfc7aad965908c74f02ab9060fd9e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-10 22:53:07 +0300

    playing with php

--- PHP/math.php
+++ PHP/math.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Math</title>
 </head>
 <body>
-    
+    <h1>AP & GP Calculations</h1>
 </body>
 </html>
