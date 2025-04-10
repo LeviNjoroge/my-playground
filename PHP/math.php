92e88e0bf526feca97a25a48b857610c3d37ffb6
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-10 22:57:19 +0300

    playing with php

--- PHP/math.php
+++ PHP/math.php
@@ -7,5 +7,19 @@
 </head>
 <body>
     <h1>AP & GP Calculations</h1>
+    <p>
+        With this site, you will be able to caclulate the following:
+        <ul>
+            <li>Sum to n</li>
+            <li>nth number</li>
+        </ul>
+        of both the AP and GP
+    </p>
+    <p>Let's begin...</p>
+    <form action="">
+        <label for="a">Enter the first number of the series/sequence:</label><br>
+        <input type="text" name="a" id="a"> <br>
+        <label for="nth">Enter tle</label>
+    </form>
 </body>
 </html>
