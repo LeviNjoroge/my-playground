d2cb25e48389c2acbf750c29d5975432488bb01e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-18 21:46:55 +0300

    workin on some math problems with php

--- PHP/math/advanced_math.php
+++ PHP/math/advanced_math.php
@@ -1,15 +1,15 @@
 <!-- 
 🔴 ADVANCED LEVEL
-    Check Prime Number
+    1. Check Prime Number
         Write a PHP function that checks whether a given number $n = 29 is a prime number.
 
-    Find All Armstrong Numbers Between 100 and 999
+    2. Find All Armstrong Numbers Between 100 and 999
         Write a script that finds and prints all 3-digit Armstrong numbers.
 
-    Solve a Quadratic Equation
+    3. Solve a Quadratic Equation
         Given coefficients $a = 1, $b = -3, and $c = 2, write a PHP script to solve the quadratic equation using the quadratic formula.
 
-    Greatest Common Divisor (GCD)
+    4. Greatest Common Divisor (GCD)
         Write a PHP function to find the GCD of two numbers, e.g., $a = 36, $b = 60. 
 -->
 
