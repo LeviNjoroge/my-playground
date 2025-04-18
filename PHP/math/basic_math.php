0678ed3439648fedcaf6f961f8291e044412762a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-18 21:42:50 +0300

    workin on some math problems with php

--- PHP/math/basic_math.php
+++ PHP/math/basic_math.php
@@ -4,8 +4,8 @@ Add Two Numbers
     Write a PHP script that adds two numbers $a = 5 and $b = 7 and prints the result.
 
 Area of a Circle
-Given a radius $r = 4, write a PHP program to calculate the area of the circle using the formula πr². (Use M_PI for π.)
+    Given a radius $r = 4, write a PHP program to calculate the area of the circle using the formula πr². (Use M_PI for π.)
 
 Check if a Number is Even or Odd
-Write a PHP script that checks if a number $n = 13 is even or odd. 
+    Write a PHP script that checks if a number $n = 13 is even or odd. 
 -->
