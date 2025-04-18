96985a7d763037a2f327a6a5e4f498e57a119517
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-18 21:44:47 +0300

    workin on some math problems with php

--- PHP/math/intermediate_math.php
+++ PHP/math/intermediate_math.php
@@ -1,11 +1,11 @@
 <!-- 
 🟡 INTERMEDIATE LEVEL
-Multiplication Table
-    Write a PHP script that prints the multiplication table of 7 from 1 to 10 using a loop.
+    Multiplication Table
+        Write a PHP script that prints the multiplication table of 7 from 1 to 10 using a loop.
 
-Factorial of a Number
-    Write a PHP function to calculate the factorial of a given number $n = 5.
+    Factorial of a Number
+        Write a PHP function to calculate the factorial of a given number $n = 5.
 
-Sum of Digits
-    Write a PHP program that takes a number $n = 1234 and returns the sum of its digits (1+2+3+4 = 10). 
+    Sum of Digits
+        Write a PHP program that takes a number $n = 1234 and returns the sum of its digits (1+2+3+4 = 10). 
 -->
