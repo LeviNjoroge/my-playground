39f7a3624762e58997013fd71d17f7d1c519e438
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-21 20:24:47 +0300

    workin on basic math with php

--- PHP/math/basic_math.php
+++ PHP/math/basic_math.php
@@ -19,6 +19,12 @@
     <title>Basic Math</title>
 </head>
 <body>
-
+    <div class="basic_math">
+    <form action="">
+        <input type="text" name="num1" id="num1" placeholder="First Number..."> <br>
+        <input type="text" name="num2" id="num2" placeholder="Second Number..."> <br>
+        <input type="button" value="Perform Basic Math">
+    </form>
+    </div>
 </body>
 </html>
