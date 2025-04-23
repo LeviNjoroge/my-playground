81f0b0e0587df82fbc13af9c647a55862de32765
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-23 19:34:04 +0300

    workin on basic math with php

--- PHP/math/basic_math.php
+++ PHP/math/basic_math.php
@@ -31,5 +31,12 @@
 </html>
 
 <?php
-    
+    if($_GET["num1"]&&$_GET["num2"]){
+        $num1 = $_GET["num1"];
+        $num2 = $_GET["num2"];
+        echo "$num1 + $num2 = ".$num1 + $num2."<br>";
+        echo "$num1 - $num2 = ".$num1 - $num2."<br>";
+        echo "$num1 x $num2 = ".$num1 * $num2."<br>";
+        echo "$num1/$num2 = ".$num1 / $num2." with the remainder is: ".$num1% $num2."<br>";
+    }
 ?>
