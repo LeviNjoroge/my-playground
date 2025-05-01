8ff700fd9c67fe46a2e92f9ae7e562dc776c3117
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-01 08:00:23 +0300

    working with mysql_dtb on php

--- PHP/phpXmysql/index.php
+++ PHP/phpXmysql/index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Soen 260 School</title>
 </head>
 <body>
-    
+    <table>
+        <thead>
+            <th>
+                <td>Registration Number</td>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Gender</td>
+                <td>Fee Paid</td>
+                <td>Course</td>
+            </th>
+        </thead>
+    </table>
 </body>
 </html>
