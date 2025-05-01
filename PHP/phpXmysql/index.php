36d6e55218d3db9d1223dab8c7faca7f0534e4ee
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-01 08:41:58 +0300

    working with mysql_dtb on php

--- PHP/phpXmysql/index.php
+++ PHP/phpXmysql/index.php
@@ -10,7 +10,21 @@
     <title>Soen 260 School</title>
     <style>
         .students_table{
-            
+            margin: 25px 0;
+            border-collapse: collapse;
+            font-size: 0.9em;
+            min-width: 720px;
+        }
+
+        .students_table thead tr{
+            background-color:#009879;
+            color: white;
+            text-align: left;
+            font-weight: bold;
+        }
+
+        .students_table td, .students_table th{
+            padding: 12px 15px;
         }
     </style>
 </head>
@@ -27,7 +41,7 @@
             </tr>
         </thead>
         <tbody>
-                <!-- <?php
+                <?php
                     while ($row = mysqli_fetch_assoc($fetch_all_result)) {
                         echo "<tr>
                                 <td>{$row['regno']}</td>
@@ -38,15 +52,15 @@
                                 <td>{$row['coursename']}</td>
                               </tr>";
                     }
-                ?> -->
-                <tr>
+                ?>
+                <!-- <tr>
                     <td>S001</td>
                     <td>Lee</td>
                     <td>Kin</td>
                     <td>M</td>
                     <td>120,000</td>
                     <td>Applied Comp</td>
-                </tr>
+                </tr> -->
         </tbody>
     </table>
 </body>
