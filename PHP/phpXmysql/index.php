bf5bef995c4ca5ed10e8280266cdf906f8f9bee8
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-01 08:05:50 +0300

    working with mysql_dtb on php

--- PHP/phpXmysql/index.php
+++ PHP/phpXmysql/index.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soen 260 School</title>
-</head>
-<body>
-    <table>
-        <thead>
-            <th>
-                <td>Registration Number</td>
-                <td>First Name</td>
-                <td>Last Name</td>
-                <td>Gender</td>
-                <td>Fee Paid</td>
-                <td>Course</td>
-            </th>
-        </thead>
-    </table>
-</body>
-</html>
+<?php
+    require "results.php";
+?>
--- PHP/phpXmysql/results.php
+++ PHP/phpXmysql/results.php
@@ -1,4 +1,8 @@
 <?php
     require 'connection.php';
     
+    $fetch_all = "SELECT regno, fname, lname, gender, fee_paid, coursename FROM students JOIN course ON students.courseid = course.coursecode;";
+
+    $fetch_all_result = mysqli_query($conn, $fetch_all);
+    
 ?>
