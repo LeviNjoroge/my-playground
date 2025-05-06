bd82c7121cc79c2dcba84eb0f2bc1e323bc47971
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-06 16:06:22 +0300

    learning springboot

---
+++ SpringBoot/not a demo/not-a-demo/src/main/java/com/anonymousleej/not/a/demo/NotADemoApplication.java
@@ -0,0 +1,12 @@
+package com.anonymousleej.not.a.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotADemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotADemoApplication.class, args);
+	}
+}
---
+++ SpringBoot/not a demo/not-a-demo/src/test/java/com/anonymousleej/not/a/demo/NotADemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymousleej.not.a.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotADemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
