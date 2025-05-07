d52378d316b1ea6a27cd92cb35be6599b92e6d99
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-07 19:42:34 +0300

    working on advanced math using PHP

--- PHP/math/advanced_math.php
+++ PHP/math/advanced_math.php
@@ -13,3 +13,14 @@
         Write a PHP function to find the GCD of two numbers, e.g., $a = 36, $b = 60. 
 -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
