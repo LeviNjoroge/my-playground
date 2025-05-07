c3b87afc49931280ce294de8ba6a2b9e5da0d09a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-07 19:46:03 +0300

    working on advanced math using PHP

--- PHP/math/advanced_math.php
+++ PHP/math/advanced_math.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Advanced \</title>
+    <title>Advanced Math</title>
 </head>
 <body>
     
