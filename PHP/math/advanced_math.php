5158fe004b328c5e651eaaaa7796c4d138338e76
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-13 18:03:12 +0300

    working on advanced math using PHP

--- PHP/math/advanced_math.php
+++ PHP/math/advanced_math.php
@@ -91,6 +91,8 @@
                 if(isset($_GET["num1"]) && isset($_GET["num2"])){
                     $num1 = $_GET["num1"];
                     $num2 = $_GET["num2"];
+
+                    
                 }
             ?>
     </div>
