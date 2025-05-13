7d213b6975e4eef00e15d169935324276680fe13
Levi Njoroge <Levynjoroge1@gmail.com>
2025-05-13 18:08:12 +0300

    working on advanced math using PHP

--- PHP/math/advanced_math.php
+++ PHP/math/advanced_math.php
@@ -97,7 +97,7 @@
                     // compare the elements in the arrays and multiply the commons, 
                     // the product is the gcd
 
-                    
+                    $prime = 0;
                     
                 }
             ?>
